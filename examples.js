706bb381101ff50798b104ad0173644d1eadac96
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-06 00:21:48 -0500

    adds plugins examples

--- examples.js
+++ examples.js
@@ -9,8 +9,9 @@ var config = new Files();
 
 function permalinks(data) {
   var interpolate = placeholders({data: data});
+
   return function fn(val) {
-    if (!val.node) return fn;
+    if (!val.filesNode) return fn;
     var data = path.parse(val.src[0]);
     val.dest = interpolate(val.dest, data);
     console.log(val.dest)
@@ -32,3 +33,19 @@ obj.forEach(function(val) {
 function arrayify(val) {
   return Array.isArray(val) ? val : [val];
 }
+
+
+var config = new Files();
+function addFoo(config) {
+  return function fn(node) {
+    if (!node.filesNode) return fn;
+    // return the plugin function if it's not a filesNode
+    // this way we know with certainty that `node`
+    // will be a filesNode
+    node.options.foo = 'bar';
+    node.dest = 'baz/';
+    node.abc = 'xyz';
+  };
+}
+config.use(addFoo);
+console.log(config.expand({src: '*.js'}).files);
